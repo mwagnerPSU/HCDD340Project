a9444f9f9c36024d441a6d58a8b8210e5234f970
mwagnerPSU <mvw5669@psu.edu>
2021-04-27 14:42:12 -0400

    styles for home page

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Image} from 'react-native';
 import Navigator from '../Navigation/Navigator';
 
 
@@ -9,7 +9,26 @@ export default function App() {
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text>Home Screen</Text>
+            <View style={styles.titleArea}>
+                <Text style={styles.title}>Welcome to Caddy</Text>
+            </View>
+            <View style={styles.middleContent}>
+                <Text style={styles.middleWords}>Caddy is a chatbot to help you</Text>
+                <Text style={styles.middleWords}>understand substance abuse</Text>
+                <Text style={styles.middleWords}>and provide help</Text>
+            </View>
+            <View style={styles.bottomContent}>
+                <View style={styles.bottomWordsArea}>
+                    <Text style={styles.caddyTopWords}>Hey I'm Caddy!</Text>
+                    <Text style={styles.caddyBottomWords}>Click the "Chatbot" tab to talk!</Text>
+                </View>
+                <View style={styles.caddyArea}>
+                    <Image
+                        source={require('../Images/Caddy2.jpg')}
+                        style={styles.caddy}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
@@ -18,7 +37,65 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: "flex-start",
         backgroundColor: '#EBF5E8'
-    }
+    },
+    titleArea: {
+        alignItems: "center",
+        marginTop: '20%',
+        marginBottom: '20%',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: '700',
+    },
+    middleContent: {
+        marginTop: '20%',
+        marginBottom: '20%',
+        width: '70%',
+        alignItems: "center"
+    },
+    middleWords: {
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    bottomContent: {
+        flexDirection: 'row',
+        marginTop: '30%',
+        marginBottom: '124%',
+        marginLeft: '20%',
+        width: '80%',
+        alignItems: "flex-start",
+        alignContent: 'space-between',
+        backgroundColor: "green",
+    },
+    bottomWordsArea: {
+        
+    },
+    caddyTopWords: {
+        fontSize: 17,
+        fontWeight: '300',
+        paddingTop: 5,
+        paddingBottom: 20,
+        paddingLeft: 5,
+    },
+    caddyBottomWords: {
+        fontSize: 17,
+        fontWeight: '300',
+        paddingTop: 5,
+        paddingLeft: 5,
+    },
+    caddyArea: {
+        flex: 1,
+        paddingLeft: 20,
+        marginLeft: 0,
+        
+    },
+    caddy: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+        paddingLeft: 0,
+        marginLeft: 0,
+    },
 });
